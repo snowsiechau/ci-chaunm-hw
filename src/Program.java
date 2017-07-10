27fa8cb59d44ebb5646c07ea8f245f71635f8e8b
snow_siechau <chautho95@gmail.com>
2017-07-10 21:33:00 +0700

    add commit

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+import game.GameWindow;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * Created by SNOW on 7/4/2017.
+ */
+public class Program {
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.run();
+    }
+
+}

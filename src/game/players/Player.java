eefb5c15dfe8f66862b8619e7f8f6d30643a4502
snow_siechau <chautho95@gmail.com>
2017-07-22 13:44:31 +0700

    Refactor Background Refactor Enemy Focus player

--- src/game/players/Player.java
+++ src/game/players/Player.java
@@ -1,46 +1,84 @@
 package game.players;
 
-import bases.Contraints;
-import bases.FrameCounter;
-import bases.ImageRenderer;
-import bases.Vector2D;
+import bases.*;
 import game.Utils;
+import game.enemies.EnemyFocus;
+import game.enemies.EnemySpellsFocus;
+import game.inputs.InputManager;
 
+import javax.xml.bind.ValidationEvent;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * Created by SNOW on 7/13/2017.
  */
-public class Player {
-
-    public Vector2D position;
-    public ImageRenderer imageRenderer;
+public class Player extends GameObjects {
     Contraints contraints;
+    InputManager inputManager;
+
+    Vector2D velocity;
 
     FrameCounter cooldownCounter;
+    FrameCounter enemyFocusCounter;
     boolean spellDisable;
 
+
     public Player(){
-        this.position = new Vector2D();
+        this.velocity = new Vector2D();
         this.cooldownCounter = new FrameCounter(17);
-        this.imageRenderer  = new ImageRenderer(Utils.loadAssetsImage("players/straight/0.png"));
+        this.enemyFocusCounter = new FrameCounter(100);
+        this.renderer = new ImageRenderer(Utils.loadAssetsImage("players/straight/0.png"));
     }
 
-    public void move(float dx, float dy){
-        this.position.addUp(dx, dy);
-        contraints.make(this.position);
+    @Override
+    public void run() {
+        move();
+
+        castSpell();
+
+        cooldown();
+
+        enemyFocus();
+
     }
 
-    public void castSpell(ArrayList<PlayerSpell> playerSpells){
-        if (!spellDisable) {
-            PlayerSpell playerSpell = new PlayerSpell();
-            playerSpell.position.set(this.position.add(0, -20));
-            playerSpells.add(playerSpell);
+    private void enemyFocus() {
+        if (enemyFocusCounter.run()) {
 
-            spellDisable = true;
+            EnemyFocus enemyFocus = new EnemyFocus(this.position);
+            float x = (int) (Math.random() * 300 + 5);
+            enemyFocus.position.set(x, 0);
+
+            GameObjects.add(enemyFocus);
+            enemyFocusCounter.reset();
         }
     }
+
+    public void move(){
+        this.velocity.set(0, 0);
+        if (inputManager.leftPressed) velocity.x -= 5;
+        if (inputManager.rightPressed) velocity.x += 5;
+        if (inputManager.upPressed) velocity.y -= 5;
+        if (inputManager.downPressed) velocity.y += 5;
+
+        this.position.addUp(velocity);
+        this.contraints.make(this.position);
+    }
+
+    public void castSpell(){
+        if (!spellDisable) {
+            if (inputManager.xPressed) {
+                PlayerSpell playerSpell = new PlayerSpell();
+                playerSpell.position.set(this.position.add(0, -20));
+                GameObjects.add(playerSpell);
+
+                spellDisable = true;
+            }
+        }
+    }
+
 
     public void cooldown(){
         if (spellDisable){
@@ -53,10 +91,15 @@ public class Player {
     }
 
     public void render(Graphics2D g2d){
-        imageRenderer.render(g2d, this.position);
+        renderer.render(g2d, this.position);
     }
 
     public void setContraints(Contraints contraints){
         this.contraints = contraints;
     }
+
+    public void setInputManager(InputManager inputManager){
+        this.inputManager = inputManager;
+    }
+
 }
